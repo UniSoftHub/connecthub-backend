b608c33205047ed611c1ebf76585a81a17ed5fb4
MrClaro <adryan.contatoprofissional@gmail.com>
2025-09-12 23:37:18 -0300

    refactor(domain/project): refatorado enum de tecnologias de projetos
    Adicionado atributo `displayName` para exibição e padronização de nomes
    exibidos

--- src/main/java/br/com/hub/connect/domain/project/enums/ProjectTechnologies.java
+++ src/main/java/br/com/hub/connect/domain/project/enums/ProjectTechnologies.java
@@ -1,30 +1,59 @@
 package br.com.hub.connect.domain.project.enums;
 
 public enum ProjectTechnologies {
-  JAVA,
-  JAVASCRIPT,
-  PYTHON,
-  RUBY,
-  PHP,
-  CSHARP,
-  CPLUSPLUS,
-  C,
-  SWIFT,
-  KOTLIN,
-  GO,
-  RUST,
-  TYPESCRIPT,
-  HTML,
-  CSS,
-  SQL,
-  NOSQL,
-  DOCKER,
-  KUBERNETES,
-  AWS,
-  AZURE,
-  GCP,
-  LINUX,
-  WINDOWS,
-  MACOS
+  JAVA("Java"),
+  JAVASCRIPT("JavaScript"),
+  PYTHON("Python"),
+  RUBY("Ruby"),
+  PHP("PHP"),
+  CSHARP("C#"),
+  CPLUSPLUS("C++"),
+  C("C"),
+  SWIFT("Swift"),
+  KOTLIN("Kotlin"),
+  GO("Go"),
+  TYPESCRIPT("TypeScript"),
+  RUST("Rust"),
+  HTML("HTML"),
+  CSS("CSS"),
+  SQL("SQL"),
+  NOSQL("NoSQL"),
+  DOCKER("Docker"),
+  KUBERNETES("Kubernetes"),
+  AWS("AWS"),
+  AZURE("Azure"),
+  GCP("GCP"),
+  LINUX("Linux"),
+  WINDOWS("Windows"),
+  MACOS("macOS"),
+  NODEJS("Node.js"),
+  REACT("React"),
+  ANGULAR("Angular"),
+  VUE("Vue.js"),
+  DJANGO("Django"),
+  FLASK("Flask"),
+  SPRING("Spring"),
+  LARAVEL("Laravel"),
+  RUBYONRAILS("Ruby on Rails"),
+  ASPNET("ASP.NET"),
+  EXPRESS("Express.js"),
+  MONGODB("MongoDB"),
+  POSTGRESQL("PostgreSQL"),
+  MYSQL("MySQL"),
+  SQLITE("SQLite"),
+  REDIS("Redis"),
+  GRAPHQL("GraphQL"),
+  REST("REST"),
+  GIT("Git");
+
+  private final String displayName;
+
+  ProjectTechnologies(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
 
 }
